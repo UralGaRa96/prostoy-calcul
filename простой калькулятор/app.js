74fcb69e56348e3affb2480e3ec1ca4d742f2095
Dmitry <99546164+UralGaRa96@users.noreply.github.com>
2022-12-23 11:46:09 +0500

    Add files via upload

--- простой калькулятор/app.js
+++ простой калькулятор/app.js
@@ -1,15 +1,15 @@
-	  // переменная, в которой хранится выбранное математическое действие //
-	  let op; 
+	  // переменная, c математическим действием  //
+	  let xui; 
 
 	  // функция расчёта //
-	  function func() {
+	  function fu() {
 	  	// переменная для результата //
 	    let result;
 	    // получаем первое и второе число //
 	    let num1 = Number(document.getElementById("num1").value);
 	    let num2 = Number(document.getElementById("num2").value);
 	    // смотрим, что было в переменной с действием, и действуем исходя из этого //
-	    switch (op) {
+	    switch (xui) {
 	      case '+':
 	        result = num1 + num2;
 	        break;
